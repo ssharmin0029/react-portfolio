365140783185ae67c9795798e70a5f936b5fa4bf
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-07-11 18:41:54 -0400

    Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,22 +9,22 @@ function Footer() {
       <nav id="content-contact" className="navbar navbar-dark bg-dark">
         <div className="container-fluid contact-section ">
           <ul className="contact-list">
-            <li>
+            <li className="contact-list-items">
               <a href="mailto:sabrina.sharmin29@myhunter.cuny.edu">
-                <FontAwesomeIcon icon={faEnvelopeSquare} size="2x" />
+                <FontAwesomeIcon icon={faEnvelopeSquare} size="3x" />
               </a>
             </li>
-            <li>
+            <li className="contact-list-items">
               <a href="https://github.com/ssharmin0029" target="_blank">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
+                <FontAwesomeIcon icon={faGithub} size="3x" />
               </a>
             </li>
-            <li>
+            <li className="contact-list-items">
               <a
                 href="https://www.linkedin.com/in/sabrina-sharmin-68171b13a/"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
               </a>
             </li>
           </ul>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Resume() {
-    return (
-        <div id="content-resume">
-        </div>
-    );
+  return (
+    <div className="container">
+      <a
+        href="./sabrina-resume.pdf"
+        className="card-link"
+        download="sabrina-resume"
+      ></a>
+    </div>
+  );
 }
 
 export default Resume;
