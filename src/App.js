fac1ba893750924d4f593d93ab040092df1fe6f1
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-07-11 05:50:49 -0400

    server.js created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import About from "./components/About";
 // import Contact from "./components/Contact";
 import Work from "./components/Work";
 import Resume from "./components/Resume";
-import Footer from './components/Footer';
-import './assets/css/reset.css'
-import './assets/css/style.css'
+import Footer from "./components/Footer";
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
 
 function App() {
   return (
     <div>
+      <Router>
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        {/* <Route exact path="/contact" component={Contact} /> */}
+        <Route exact path="/work" component={Work} />
+        <Route path="/resume" component={Resume} />
+      </Router>
 
-    <Router>
-      <NavTabs />
-      <Route exact path="/" component={About} />
-      {/* <Route exact path="/contact" component={Contact} /> */}
-      <Route exact path="/work" component={Work} />
-      <Route path="/resume" component={Resume} />
-    </Router>
-
-    <Footer />
+      <Footer />
     </div>
   );
 }
